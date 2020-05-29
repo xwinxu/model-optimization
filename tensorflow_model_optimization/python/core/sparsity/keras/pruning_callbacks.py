c7b1cb1524b68a236d299e164e9cd8488cf63569
Winnie Xu <winnie.xu97@gmail.com>
2020-05-29 13:37:02 -0400

    callback draft

--- tensorflow_model_optimization/python/core/sparsity/keras/pruning_callbacks.py
+++ tensorflow_model_optimization/python/core/sparsity/keras/pruning_callbacks.py
@@ -30,117 +30,174 @@ callbacks = tf.keras.callbacks
 
 
 def _collect_prunable_layers(model):
-  """Recursively collect the prunable layers in the model."""
-  prunable_layers = []
-  for layer in model.layers:
-    # A keras model may have other models as layers.
-    if isinstance(layer, tf.keras.Model):
-      prunable_layers += _collect_prunable_layers(layer)
-    if isinstance(layer, pruning_wrapper.PruneLowMagnitude):
-      prunable_layers.append(layer)
+    """Recursively collect the prunable layers in the model."""
+    prunable_layers = []
+    for layer in model.layers:
+        # A keras model may have other models as layers.
+        if isinstance(layer, tf.keras.Model):
+            prunable_layers += _collect_prunable_layers(layer)
+        if isinstance(layer, pruning_wrapper.PruneLowMagnitude):
+            prunable_layers.append(layer)
 
-  return prunable_layers
+    return prunable_layers
 
 
 class UpdatePruningStep(callbacks.Callback):
-  """Keras callback which updates pruning wrappers with the optimizer step.
+    """Keras callback which updates pruning wrappers with the optimizer step.
 
-  This callback must be used when training a model which needs to be pruned. Not
-  doing so will throw an error.
+    This callback must be used when training a model which needs to be pruned. Not
+    doing so will throw an error.
 
-  Example:
+    Example:
 
-  ```python
-  model.fit(x, y,
-      callbacks=[UpdatePruningStep()])
-  ```
-  """
+    ```python
+    model.fit(x, y,
+        callbacks=[UpdatePruningStep()])
+    ```
+    """
 
-  def __init__(self):
-    super(UpdatePruningStep, self).__init__()
-    self.prunable_layers = []
+    def __init__(self):
+        super(UpdatePruningStep, self).__init__()
+        self.prunable_layers = []
 
-  def on_train_begin(self, logs=None):
-    # Collect all the prunable layers in the model.
-    self.prunable_layers = _collect_prunable_layers(self.model)
-    self.step = K.get_value(self.model.optimizer.iterations)
+    def on_train_begin(self, logs=None):
+        # Collect all the prunable layers in the model.
+        self.prunable_layers = _collect_prunable_layers(self.model)
+        self.step = K.get_value(self.model.optimizer.iterations)
 
-  def on_train_batch_begin(self, batch, logs=None):
-    tuples = []
-    for layer in self.prunable_layers:
-      tuples.append((layer.pruning_step, self.step))
+    def on_train_batch_begin(self, batch, logs=None):
+        tuples = []
+        for layer in self.prunable_layers:
+            tuples.append((layer.pruning_step, self.step))
 
-    K.batch_set_value(tuples)
-    self.step = self.step + 1
+        K.batch_set_value(tuples)
+        self.step = self.step + 1
 
-  def on_epoch_end(self, batch, logs=None):
-    # At the end of every epoch, remask the weights. This ensures that when
-    # the model is saved after completion, the weights represent mask*weights.
-    weight_mask_ops = []
+    def on_epoch_end(self, batch, logs=None):
+        # At the end of every epoch, remask the weights. This ensures that when
+        # the model is saved after completion, the weights represent mask*weights.
+        weight_mask_ops = []
 
-    for layer in self.prunable_layers:
-      if isinstance(layer, pruning_wrapper.PruneLowMagnitude):
-        if tf.executing_eagerly():
-          layer.pruning_obj.weight_mask_op()
-        else:
-          weight_mask_ops.append(layer.pruning_obj.weight_mask_op())
+        for layer in self.prunable_layers:
+            if isinstance(layer, pruning_wrapper.PruneLowMagnitude):
+                if tf.executing_eagerly():
+                    layer.pruning_obj.weight_mask_op()
+                else:
+                    weight_mask_ops.append(layer.pruning_obj.weight_mask_op())
 
-    K.batch_get_value(weight_mask_ops)
+        K.batch_get_value(weight_mask_ops)
 
 
 class PruningSummaries(callbacks.TensorBoard):
-  """A Keras callback for adding pruning summaries to tensorboard.
+    """A Keras callback for adding pruning summaries to tensorboard.
 
-  Logs the sparsity(%) and threshold at a given iteration step.
-  """
+    Logs the sparsity(%) and threshold at a given iteration step.
+    """
 
-  def __init__(self, log_dir, update_freq='epoch', **kwargs):
-    super(PruningSummaries, self).__init__(
-        log_dir=log_dir, update_freq=update_freq, **kwargs)
+    def __init__(self, log_dir, update_freq='epoch', **kwargs):
+        super(PruningSummaries, self).__init__(
+            log_dir=log_dir, update_freq=update_freq, **kwargs)
 
-  def _log_pruning_metrics(self, logs, prefix, step):
-    if compat.is_v1_apis():
-      # Safely depend on TF 1.X private API given
-      # no more 1.X releases.
-      self._write_custom_summaries(step, logs)
-    else:  # TF 2.X
-      log_dir = self.log_dir + '/metrics'
+    def _log_pruning_metrics(self, logs, prefix, step):
+        if compat.is_v1_apis():
+            # Safely depend on TF 1.X private API given
+            # no more 1.X releases.
+            self._write_custom_summaries(step, logs)
+        else:  # TF 2.X
+            log_dir = self.log_dir + '/metrics'
 
-      file_writer = tf.summary.create_file_writer(log_dir)
-      file_writer.set_as_default()
+            file_writer = tf.summary.create_file_writer(log_dir)
+            file_writer.set_as_default()
 
-      for name, value in logs.items():
-        tf.summary.scalar(name, value, step=step)
+            for name, value in logs.items():
+                tf.summary.scalar(name, value, step=step)
 
-      file_writer.flush()
+            file_writer.flush()
 
-  def on_epoch_begin(self, epoch, logs=None):
-    if logs is not None:
-      super(PruningSummaries, self).on_epoch_begin(epoch, logs)
+    def on_epoch_begin(self, epoch, logs=None):
+        if logs is not None:
+            super(PruningSummaries, self).on_epoch_begin(epoch, logs)
 
-    pruning_logs = {}
-    params = []
-    prunable_layers = _collect_prunable_layers(self.model)
-    for layer in prunable_layers:
-      for _, mask, threshold in layer.pruning_vars:
-        params.append(mask)
-        params.append(threshold)
+        pruning_logs = {}
+        params = []
+        prunable_layers = _collect_prunable_layers(self.model)
+        for layer in prunable_layers:
+            for _, mask, threshold in layer.pruning_vars:
+                params.append(mask)
+                params.append(threshold)
 
-    params.append(self.model.optimizer.iterations)
+        params.append(self.model.optimizer.iterations)
 
-    values = K.batch_get_value(params)
-    iteration = values[-1]
-    del values[-1]
-    del params[-1]
+        values = K.batch_get_value(params)
+        iteration = values[-1]
+        del values[-1]
+        del params[-1]
 
-    param_value_pairs = list(zip(params, values))
+        param_value_pairs = list(zip(params, values))
 
-    for mask, mask_value in param_value_pairs[::2]:
-      pruning_logs.update({
-          mask.name + '/sparsity': 1 - np.mean(mask_value)
-      })
+        for mask, mask_value in param_value_pairs[::2]:
+            pruning_logs.update({
+                mask.name + '/sparsity': 1 - np.mean(mask_value)
+            })
 
-    for threshold, threshold_value in param_value_pairs[1::2]:
-      pruning_logs.update({threshold.name + '/threshold': threshold_value})
+        for threshold, threshold_value in param_value_pairs[1::2]:
+            pruning_logs.update(
+                {threshold.name + '/threshold': threshold_value})
 
-    self._log_pruning_metrics(pruning_logs, '', iteration)
+        self._log_pruning_metrics(pruning_logs, '', iteration)
+
+
+class LTHPruningStep(callbacks.Callback):
+    """Keras callback which updates pruning wrappers with the optimizer step.
+
+    This callback must be used when training a model which needs to be pruned. Not
+    doing so will throw an error.
+
+    Example:
+
+    ```python
+    model.fit(x, y,
+        callbacks=[LTHPruningStep()])
+    ```
+    """
+
+    def __init__(self, ckpt_path, reset_itr=0, end_step=-1):
+        super(LTHPruningStep, self).__init__()
+        self.prunable_layers = []
+        self.ckpt_path = ckpt_path
+        # our "initialization" can be reset to any generic state in training
+        self.reset_step = reset_step
+        self.end_step = end_step
+        # weights to re-initialize to
+        self.lottery_tix = None
+
+    def on_train_begin(self, logs=None):
+        # Collect all the prunable layers in the model.
+        self.prunable_layers = _collect_prunable_layers(self.model)
+        self.step = K.get_value(self.model.optimizer.iterations)
+
+        # save model parameters at correct step
+        if self.step == self.reset_step:
+            self.lottery_tix = self.model.get_weights()
+
+    def on_train_batch_begin(self, batch, logs=None):
+        tuples = []
+        for layer in self.prunable_layers:
+            tuples.append((layer.pruning_step, self.step))
+
+        K.batch_set_value(tuples)
+        self.step = self.step + 1
+
+    def on_epoch_end(self, batch, logs=None):
+        # At the end of every epoch, remask the weights. This ensures that when
+        # the model is saved after completion, the weights represent mask*weights.
+        weight_mask_ops = []
+
+        for layer in self.prunable_layers:
+            if isinstance(layer, pruning_wrapper.PruneLowMagnitude):
+                if tf.executing_eagerly():
+                    layer.pruning_obj.weight_mask_op()
+                else:
+                    weight_mask_ops.append(layer.pruning_obj.weight_mask_op())
+
+        K.batch_get_value(weight_mask_ops)
