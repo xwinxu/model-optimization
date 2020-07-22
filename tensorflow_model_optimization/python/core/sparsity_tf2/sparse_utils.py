697208dcc93f0afd367a1e9943dbbfa21667d182
Winnie Xu <xwinxu@google.com>
2020-07-22 13:55:00 +0000

    delete embedded repo

--- tensorflow_model_optimization/python/core/sparsity_tf2/sparse_utils.py
+++ tensorflow_model_optimization/python/core/sparsity_tf2/sparse_utils.py
@@ -1,19 +1,3 @@
-def uniform(weights, optimizer, sparsity, pruning_config, extract_name_fn):
-  mask_to_sparsity = {}
-  for weight in weights:
-    weight_pruner = pruning_config.get_pruner(weight)
-    weight_name = weight.name
-
-  # TODO
-  return 
-
-def erdos_renyi():
-  return
-
-def erdos_renyi_kernel():
-  return
-
-
 class Bernouilli(tf.keras.initializers.Initializer):
   """
   Mask initialization distribution.
@@ -41,3 +25,21 @@ class Bernouilli(tf.keras.initializers.Initializer):
     uniform = tf.random.uniform(shape)
     mask = tf.less(uniform, probs)
     return tf.cast(mask, tf.float32)
+
+
+def uniform(weights, optimizer, sparsity, pruning_config, extract_name_fn):
+  mask_sampler = Bernouilli(p=sparsity)
+  
+
+
+  # TODO
+  return 
+
+def erdos_renyi():
+  return
+
+def erdos_renyi_kernel():
+  return
+
+
+
